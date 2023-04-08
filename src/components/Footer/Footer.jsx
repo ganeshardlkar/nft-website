5175ef90d20765b8f6a472518965669b5c5d4f30
ganeshardlkar <ganeshardalkar5@gmail.com>
2023-04-08 15:55:44 +0530

    update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -148,7 +148,7 @@ const Footer = () => {
                     <Col lg="12" className=" mt-4 text-center">
                         <p className="copyright">
                             {" "}
-                            Copyrights 2022, Developed by Ganesh Ardalkar.
+                            Copyrights 2023, Developed by Ganesh Ardalkar.
                             All Rights Reserved.{" "}
                         </p>
                     </Col>
